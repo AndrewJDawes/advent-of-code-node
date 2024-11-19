c676de07b560a843b8612f3b488d6034812a2598
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-11-19 15:50:56 +0000

    Rename 20161 to 20161a

--- src/Solution/Factory/FromConfig.ts
+++ src/Solution/Factory/FromConfig.ts
@@ -3,7 +3,7 @@ import InterfaceInputFetcherFactory from '../../InputFetcher/Interface/Factory.j
 import InterfaceConfig from '../Interface/Config.js';
 import InterfaceSolutionStrategy from '../../Solution/Interface/Strategy.js';
 import Solution20151 from '../Strategy/2015/1.js';
-import Solution20161 from '../Strategy/2016/1.js';
+import Solution20161a from '../Strategy/2016/1a.js';
 class FromConfig implements InterfaceSolutionFactory {
     inputFetcherFactory: InterfaceInputFetcherFactory;
     constructor(inputFetcherFactory: InterfaceInputFetcherFactory) {
@@ -18,8 +18,8 @@ class FromConfig implements InterfaceSolutionFactory {
         switch (config.year + config.day) {
             case '20151':
                 return new Solution20151(service);
-            case '20161':
-                return new Solution20161(service);
+            case '20161a':
+                return new Solution20161a(service);
             default:
                 throw new Error(
                     'Unknown year and day: ' + config.year + config.day
