b0f04280241fd3b3fe6ac9d07d7b9b3a38a92cf9
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-26 12:54:14 +0000

    Remove timer

--- src/Solution/Strategy/2016/5/b.spec.ts
+++ src/Solution/Strategy/2016/5/b.spec.ts
@@ -4,13 +4,11 @@ import Solution20165b from './b.js';
 describe('Solution20165b', () => {
     describe('solve', () => {
         it('interprets ugkcyxxp', async () => {
-            console.time();
             const input = new StringArray([
                 'ugkcyxxp', // real +123
             ]);
             const solution = await new Solution20165b(input).solve();
             expect(solution).to.equal('f2c730e5');
-            console.timeEnd();
         }).timeout(0);
     });
 });
