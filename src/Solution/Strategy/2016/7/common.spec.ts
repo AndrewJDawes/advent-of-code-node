066f83557c1f0a0626d068ed2def9d0b10b3bc50
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-05-07 11:01:40 +0000

    Prototype ProtocolValidatorSSL

--- src/Solution/Strategy/2016/7/common.spec.ts
+++ src/Solution/Strategy/2016/7/common.spec.ts
@@ -1,7 +1,10 @@
 import { expect } from 'chai';
-import { AutonomousBridgeBypassAnnotationCounter } from './common.js';
+import {
+    AutonomousBridgeBypassAnnotationCounter,
+    ProtocolValidatorSSL,
+} from './common.js';
 describe('AutonomousBridgeBypassAnnotationCounter', () => {
-    it('interprets abba[mnop]qrst as 1 outside and 0 inside', async () => {
+    it('interprets abba[mnop]qrst as 1 outside and 0 inside', () => {
         const input = 'abba[mnop]qrst';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -10,7 +13,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         expect(counter.getOutsideHypernetCount()).to.equal(1);
         expect(counter.getInsideHypernetCount()).to.equal(0);
     });
-    it('interprets abba[mnop]qrst as supports ABBA protocol,', async () => {
+    it('interprets abba[mnop]qrst as supports ABBA protocol,', () => {
         const input = 'abba[mnop]qrst';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -18,7 +21,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         });
         expect(counter.supportsABBAProtocol()).to.equal(true);
     });
-    it('interprets abcd[bddb]xyyx as 1 outside and 1 inside', async () => {
+    it('interprets abcd[bddb]xyyx as 1 outside and 1 inside', () => {
         const input = 'abcd[bddb]xyyx';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -27,7 +30,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         expect(counter.getOutsideHypernetCount()).to.equal(1);
         expect(counter.getInsideHypernetCount()).to.equal(1);
     });
-    it('interprets abcd[bddb]xyyx as does not support ABBA protocol', async () => {
+    it('interprets abcd[bddb]xyyx as does not support ABBA protocol', () => {
         const input = 'abcd[bddb]xyyx';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -35,7 +38,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         });
         expect(counter.supportsABBAProtocol()).to.equal(false);
     });
-    it('interprets aaaaa[qwer]tyui as 0 outside and 0 inside', async () => {
+    it('interprets aaaaa[qwer]tyui as 0 outside and 0 inside', () => {
         const input = 'aaaaa[qwer]tyui';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -44,7 +47,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         expect(counter.getOutsideHypernetCount()).to.equal(0);
         expect(counter.getInsideHypernetCount()).to.equal(0);
     });
-    it('interprets ioxxoj[asdfgh]zxcvbn as 1 outside and 0 inside', async () => {
+    it('interprets ioxxoj[asdfgh]zxcvbn as 1 outside and 0 inside', () => {
         const input = 'ioxxoj[asdfgh]zxcvbn';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -53,7 +56,7 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         expect(counter.getOutsideHypernetCount()).to.equal(1);
         expect(counter.getInsideHypernetCount()).to.equal(0);
     });
-    it('interprets ioxxoj[asdfgh]zxcvbnnbsfs as 2 outside and 0 inside', async () => {
+    it('interprets ioxxoj[asdfgh]zxcvbnnbsfs as 2 outside and 0 inside', () => {
         const input = 'ioxxoj[asdfgh]zxcvbnnbsfs';
         const counter = new AutonomousBridgeBypassAnnotationCounter();
         input.split('').forEach((character) => {
@@ -61,5 +64,46 @@ describe('AutonomousBridgeBypassAnnotationCounter', () => {
         });
         expect(counter.getOutsideHypernetCount()).to.equal(2);
         expect(counter.getInsideHypernetCount()).to.equal(0);
+    });
+});
+/*
+
+aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
+xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+*/
+describe('ProtocolValidatorSSL', () => {
+    it('interprets aba[bab]xyz as supports SSL', () => {
+        const input = 'aba[bab]xyz';
+        const validator = new ProtocolValidatorSSL();
+        input.split('').forEach((character) => {
+            validator.add(character);
+        });
+        expect(validator.isValid()).to.equal(true);
+    });
+    it('interprets xyx[xyx]xyx as does not support SSL', () => {
+        const input = 'xyx[xyx]xyx';
+        const validator = new ProtocolValidatorSSL();
+        input.split('').forEach((character) => {
+            validator.add(character);
+        });
+        expect(validator.isValid()).to.equal(false);
+    });
+    it('interprets aaaa[kek]eke as supports SSL', () => {
+        const input = 'aaaa[kek]eke';
+        const validator = new ProtocolValidatorSSL();
+        input.split('').forEach((character) => {
+            validator.add(character);
+        });
+        expect(validator.isValid()).to.equal(true);
+    });
+    it('interprets zazbz[bzb]cdb as supports SSL', () => {
+        const input = 'zazbz[bzb]cdb';
+        const validator = new ProtocolValidatorSSL();
+        input.split('').forEach((character) => {
+            validator.add(character);
+        });
+        expect(validator.isValid()).to.equal(true);
     });
 });
--- src/Solution/Strategy/2016/7/common.ts
+++ src/Solution/Strategy/2016/7/common.ts
@@ -49,3 +49,68 @@ export class AutonomousBridgeBypassAnnotationCounter {
         }
     }
 }
+
+export class ProtocolValidatorSSL {
+    private abaSequencesNormalized: Set<string>;
+    private babSequencesNormalized: Set<string>;
+    private intersectingNormalizedSequences: Set<string>;
+    private window: string[];
+    private isHypernetSequence: boolean;
+    constructor() {
+        this.abaSequencesNormalized = new Set();
+        this.babSequencesNormalized = new Set();
+        this.intersectingNormalizedSequences = new Set();
+        this.window = [];
+        this.isHypernetSequence = false;
+    }
+    public isValid(): boolean {
+        return this.intersectingNormalizedSequences.size > 0;
+    }
+    public add(character: string) {
+        if (!this.isHypernetSequence && character === '[') {
+            this.isHypernetSequence = true;
+            this.window.splice(0, this.window.length);
+            return;
+        }
+        if (this.isHypernetSequence && character === ']') {
+            this.isHypernetSequence = false;
+            this.window.splice(0, this.window.length);
+            return;
+        }
+        this.window.push(character);
+        if (this.window.length >= 3) {
+            const windowChars = [...this.window];
+            this.window.shift();
+            if (
+                windowChars[0] === windowChars[2] &&
+                windowChars[0] !== windowChars[1]
+            ) {
+                const normalizedSequence = this.isHypernetSequence
+                    ? [windowChars[1], windowChars[0], windowChars[1]].join('')
+                    : windowChars.join('');
+                if (
+                    this.intersectingNormalizedSequences.has(normalizedSequence)
+                ) {
+                    return;
+                }
+                if (this.isHypernetSequence) {
+                    this.babSequencesNormalized.add(normalizedSequence);
+                } else {
+                    this.abaSequencesNormalized.add(normalizedSequence);
+                }
+                let newIntersections = new Set(
+                    [...this.abaSequencesNormalized].filter((x) =>
+                        this.babSequencesNormalized.has(x)
+                    )
+                );
+                for (const intersectingSequence of newIntersections) {
+                    this.intersectingNormalizedSequences.add(
+                        intersectingSequence
+                    );
+                    this.abaSequencesNormalized.delete(intersectingSequence);
+                    this.babSequencesNormalized.delete(intersectingSequence);
+                }
+            }
+        }
+    }
+}
