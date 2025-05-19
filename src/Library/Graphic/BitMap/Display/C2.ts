bda0299bb23f9bb8051339f234703f5457506e80
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-05-19 11:13:34 +0000

    Build passing test

--- src/Library/Graphic/BitMap/Display/C2.ts
+++ src/Library/Graphic/BitMap/Display/C2.ts
@@ -10,27 +10,398 @@ TODO
 - Build a 2 dimensional array per letter
 - Compare each element in that 2 dimensional array to the dictionary to translate
 
--####..##...##..###...##..###..#..#.#...#.##...##..
--#....#..#.#..#.#..#.#..#.#..#.#..#.#...##..#.#..#.
--###..#..#.#..#.#..#.#....#..#.####..#.#.#..#.#..#.
--#....#..#.####.###..#.##.###..#..#...#..####.#..#.
--#....#..#.#..#.#.#..#..#.#....#..#...#..#..#.#..#.
--####..##..#..#.#..#..###.#....#..#...#..#..#..##..
+####..##...##..###...##..###..#..#.#...#.##...##..
+#....#..#.#..#.#..#.#..#.#..#.#..#.#...##..#.#..#.
+###..#..#.#..#.#..#.#....#..#.####..#.#.#..#.#..#.
+#....#..#.####.###..#.##.###..#..#...#..####.#..#.
+#....#..#.#..#.#.#..#..#.#....#..#...#..#..#.#..#.
+####..##..#..#.#..#..###.#....#..#...#..#..#..##..
+
+#...#
+#...#
+.#.#.
+..#..
+..#..
+..#..
 
 */
 interface CharacterTranslation {
     from: string;
     to: string;
 }
-const knownLetters: CharacterTranslation[] = [
+
+export const knownLetters: CharacterTranslation[] = [
+    // A  (already in your list)
     {
-        from: `####.
-        #....
-        ###..
-        #....
-        #....
-        ####.`,
+        // prettier-ignore
+        from: [
+        '.##..',
+        '#..#.',
+        '#..#.',
+        '####.',
+        '#..#.',
+        '#..#.',
+      ].join('\n'),
+        to: 'A',
+    },
+
+    // B
+    {
+        // prettier-ignore
+        from: [
+        '###..',
+        '#..#.',
+        '#..#.',
+        '###..',
+        '#..#.',
+        '###..',
+      ].join('\n'),
+        to: 'B',
+    },
+
+    // C
+    {
+        // prettier-ignore
+        from: [
+        '.###.',
+        '#...#',
+        '#....',
+        '#....',
+        '#...#',
+        '.###.',
+      ].join('\n'),
+        to: 'C',
+    },
+
+    // D
+    {
+        // prettier-ignore
+        from: [
+        '###..',
+        '#..#.',
+        '#..#.',
+        '#..#.',
+        '#..#.',
+        '###..',
+      ].join('\n'),
+        to: 'D',
+    },
+
+    // E  (already in your list)
+    {
+        // prettier-ignore
+        from: [
+        '####.',
+        '#....',
+        '###..',
+        '#....',
+        '#....',
+        '####.',
+      ].join('\n'),
         to: 'E',
+    },
+
+    // F
+    {
+        // prettier-ignore
+        from: [
+        '####.',
+        '#....',
+        '###..',
+        '#....',
+        '#....',
+        '#....',
+        ].join('\n'),
+        to: 'F',
+    },
+
+    // G (you already have this one – shown here just for completeness)
+    {
+        // prettier-ignore
+
+        from: [
+        '.##..',
+        '#..#.',
+        '#....',
+        '#.##.',
+        '#..#.',
+        '.###.',
+        ].join('\n'),
+        to: 'G',
+    },
+
+    // H (already supplied)
+    {
+        // prettier-ignore
+
+        from: [
+            '#..#.',
+            '#..#.',
+            '####.',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+          ].join('\n'),
+        to: 'H',
+    },
+
+    // I
+    {
+        from: ['.###.', '..#..', '..#..', '..#..', '..#..', '.###.'].join('\n'),
+        to: 'I',
+    },
+
+    // J
+    {
+        // prettier-ignore
+
+        from: [
+            '..###',
+            '...#.',
+            '...#.',
+            '...#.',
+            '#..#.',
+            '.##..',
+          ].join('\n'),
+        to: 'J',
+    },
+
+    // K
+    {
+        // prettier-ignore
+
+        from: [
+            '#..#.',
+            '#.#..',
+            '##...',
+            '#.#..',
+            '#.#..',
+            '#..#.',
+          ].join('\n'),
+        to: 'K',
+    },
+
+    // L
+    {
+        // prettier-ignore
+
+        from: [
+            '#....',
+            '#....',
+            '#....',
+            '#....',
+            '#....',
+            '####.',
+          ].join('\n'),
+        to: 'L',
+    },
+    // M
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '##.##',
+            '#.#.#',
+            '#...#',
+            '#...#',
+            '#...#',
+          ].join('\n'),
+        to: 'M',
+    },
+
+    // N
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '##..#',
+            '#.#.#',
+            '#..##',
+            '#...#',
+            '#...#',
+          ].join('\n'),
+        to: 'N',
+    },
+
+    // O  (already in your list)
+    {
+        // prettier-ignore
+
+        from: [
+            '.##..',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '.##..',
+          ].join('\n'),
+        to: 'O',
+    },
+
+    // P  (already in your list)
+    {
+        // prettier-ignore
+
+        from: [
+            '###..',
+            '#..#.',
+            '#..#.',
+            '###..',
+            '#....',
+            '#....',
+          ].join('\n'),
+        to: 'P',
+    },
+
+    // Q
+    {
+        // prettier-ignore
+
+        from: [
+            '.##..',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '#.#..',
+            '.#.#.',
+          ].join('\n'),
+        to: 'Q',
+    },
+
+    // R  (already in your list)
+    {
+        // prettier-ignore
+
+        from: [
+            '###..',
+            '#..#.',
+            '#..#.',
+            '###..',
+            '#.#..',
+            '#..#.',
+          ].join('\n'),
+        to: 'R',
+    },
+    // S
+    {
+        // prettier-ignore
+
+        from: [
+            '.###.',
+            '#....',
+            '.##..',
+            '...#.',
+            '#...#',
+            '.###.',
+          ].join('\n'),
+        to: 'S',
+    },
+
+    // T
+    {
+        // prettier-ignore
+
+        from: [
+            '#####',
+            '..#..',
+            '..#..',
+            '..#..',
+            '..#..',
+            '..#..',
+          ].join('\n'),
+        to: 'T',
+    },
+
+    // U
+    {
+        // prettier-ignore
+
+        from: [
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '#..#.',
+            '.##..',
+          ].join('\n'),
+        to: 'U',
+    },
+
+    // V
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '#...#',
+            '#...#',
+            '#...#',
+            '.#.#.',
+            '..#..',
+          ].join('\n'),
+        to: 'V',
+    },
+
+    // W
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '#...#',
+            '#.#.#',
+            '#.#.#',
+            '##.##',
+            '#...#',
+          ].join('\n'),
+        to: 'W',
+    },
+
+    // X
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '.#.#.',
+            '..#..',
+            '..#..',
+            '.#.#.',
+            '#...#',
+          ].join('\n'),
+        to: 'X',
+    },
+
+    // Y  (already in your list)
+    {
+        // prettier-ignore
+
+        from: [
+            '#...#',
+            '#...#',
+            '.#.#.',
+            '..#..',
+            '..#..',
+            '..#..',
+          ].join('\n'),
+        to: 'Y',
+    },
+
+    // Z
+    {
+        // prettier-ignore
+
+        from: [
+            '#####',
+            '...#.',
+            '..#..',
+            '.#...',
+            '#....',
+            '#####',
+          ].join('\n'),
+        to: 'Z',
     },
 ];
 
@@ -38,14 +409,17 @@ export default class Display<T, B> implements DisplayInterface<T, B> {
     private pixelMap: PixelMap<T, B>;
     private characterWidth: number;
     private characterHeight: number;
-    constructor(
-        pixelMap: PixelMap<T, B>,
-        characterWidth: number,
-        characterHeight: number
-    ) {
-        this.pixelMap = pixelMap;
-        this.characterWidth = characterWidth;
-        this.characterHeight = characterHeight;
+    private dictionary: CharacterTranslation[];
+    constructor(props: {
+        pixelMap: PixelMap<T, B>;
+        characterWidth: number;
+        characterHeight: number;
+        dictionary: CharacterTranslation[];
+    }) {
+        this.pixelMap = props.pixelMap;
+        this.characterWidth = props.characterWidth;
+        this.characterHeight = props.characterHeight;
+        this.dictionary = props.dictionary;
     }
     public getCharacterStartAndEndIndexes(characterIndex: number) {
         const charsPerRow = this.getCharactersPerRowCount();
@@ -104,9 +478,22 @@ export default class Display<T, B> implements DisplayInterface<T, B> {
     }
     print(printChar: string, printBlankChar: string) {
         let chars: string[][][] = [];
-        for (let i = 0; i < this.getTotalRowsCount(); i++) {
+        for (let i = 0; i < this.getTotalCharactersCount(); i++) {
             chars.push(this.getCharacter(i, printChar, printBlankChar));
         }
-        return chars.map((row) => row.join('')).join('\n');
+        return chars
+            .map((char) => char.map((row) => row.join('')).join('\n'))
+            .map((char) => {
+                const index = this.dictionary.findIndex(
+                    (translation) => translation.from === char
+                );
+                if (-1 === index) {
+                    throw new Error(
+                        `Unable to find character ${char} in dictionary`
+                    );
+                }
+                return this.dictionary[index].to;
+            })
+            .join('');
     }
 }
