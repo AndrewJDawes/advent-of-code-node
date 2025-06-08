bc9f4e23b7d0fe9c2eb10ff0ee8cf4d071aa175f
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-06-08 18:59:42 +0000

    Scaffold out A solution

---
+++ src/Solution/Strategy/2016/10/a.spec.ts
@@ -0,0 +1,4 @@
+import { expect } from 'chai';
+import StringArray from '../../../../InputFetcher/Service/StringArray.js';
+import Solution201610a from './a.js';
+describe('Solution201610a', () => {});
